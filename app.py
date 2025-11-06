67f9f25396a33c5ed0426f06d42ed5bd1e1337ea
prkumar330s-sys <prkumar330s@gmail.com>
2025-11-07 02:33:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,7 @@ def home():
     return "CinemaSquad App Running!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
+
